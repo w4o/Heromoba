58c9b69e2d9b837b104f3794c6b8a371d26d5789
Frank <zhwuzhuo@gmail.com>
2017-01-13 16:00:28 +0800

    Init

---
+++ app-utils/src/main/java/hero/moba/utils/file/FileUtils.java
@@ -0,0 +1,46 @@
+package hero.moba.utils.file;
+
+import java.io.*;
+
+public class FileUtils {
+
+	/**
+	 * 写文件
+	 * 
+	 * @author Frank
+	 * @param fileName
+	 * @param context
+	 * @throws Exception
+	 */
+	public static void write(String fileName, String context) throws Exception {
+		File file = new File(fileName);
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		BufferedWriter br = new BufferedWriter(new FileWriter(file));
+		br.write(context);
+		br.flush();
+		br.close();
+	}
+
+	/**
+	 * 读文件
+	 * 
+	 * @author Frank
+	 * @param file
+	 * @return
+	 * @throws Exception
+	 */
+	public static String read(String file) throws Exception {
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		StringBuffer buffer = new StringBuffer();
+		String line = "";
+		while ((line = br.readLine()) != null) {
+			buffer.append(line);
+		}
+		br.close();
+
+		return buffer.toString();
+	}
+
+}
